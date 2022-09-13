bfff490c40c7f3f9665d61c4c6a8943e797d4e21
Luu Thanh Tan <hyunumau@gmail.com>
2022-09-13 11:23:00 +0700

    heroku

--- src/components/products/productCard.js
+++ src/components/products/productCard.js
@@ -12,21 +12,31 @@ function ProductCard({ productProp }) {
                     <CardContent sx={{ height: "500px" }}>
                         <img src={getUrl(productProp.image)} alt={productProp.caption} style={{ objectFit: "fill", width: "100%", height: "225px" }}></img>
                         <Grid margin={1}>
-                            <Typography gutterBottom padding={2} fontSize={20} fontWeight={600} style={{ whiteSpace: "nowrap", textOverflow: "ellipsis", overflow: "hidden" }}>
+                            <Typography gutterBottom padding={2} fontSize={20} fontWeight={600}
+                                style={{
+                                    maxWidth: '100%',
+                                    maxHeight: '80px',
+                                    textOverflow: "ellipsis",
+                                    overflow: "hidden",
+                                    display: '-webkit-box',
+                                    WebkitBoxOrient: 'vertical',
+                                    WebkitLineClamp: 2,
+                                }}
+                            >
                                 {productProp.caption}
                             </Typography>
                         </Grid>
                         <Grid sx={{ height: "150px" }}>
-                            <Typography style={{
-                                maxWidth: '100%',
-                                maxHeight: '115px',
-                                display: '-webkit-box',
-                                WebkitBoxOrient: 'vertical',
-                                WebkitLineClamp: 5,
-                                overflow: 'hidden',
-                                textOverflow: 'ellipsis',
-                            }}
-                                dangerouslySetInnerHTML={{ __html: productProp.detail }}></Typography>
+                                <Typography style={{
+                                    maxWidth: '100%',
+                                    maxHeight: '115px',
+                                    display: '-webkit-box',
+                                    WebkitBoxOrient: 'vertical',
+                                    WebkitLineClamp: 5,
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                }}
+                                    dangerouslySetInnerHTML={{ __html: productProp.detail }}></Typography>
                         </Grid>
                         <Grid>
                             <Typography sx={{ fontWeight: 600, fontSize: 14 }}>{productProp.author_info ? productProp.author_info.name : null}</Typography>
